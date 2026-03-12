f7a18c2d2bda131607f66fffb3e30be56bc06cb5
deltadev-rsc <deltaqxq174@mail.ru>
2026-03-12 20:42:18 +0500

    Create files.h

---
+++ shell/src/lib/files.h
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+#ifdef _WIN32
+    #include <direct.h>
+    #define mkdir(path, mode) _mkdir(path)
+    #define chdir _chdir
+    #define getcwd _getcwd
+#else
+    #include <unistd.h>
+    #include <sys/stat.h>
+    #include <dirent.h>
+    #include <stdlib.h>
+#endif
+
+#ifndef FILES_H
+#define FILES_H
+
+#define MFNL 256 //maximum file name length
+#define MAX_TOKEN_LENGTH 1024
+#define MAX_FOLDER_NAME_LENGTH 1024
+#define MAX_LINES 512
+#define MAX_LINE_LENGTH 1024
+#define MAX_FOLDER_LENGTH 128
+#define MAX_PATH_LENGTH 128
+
+typedef struct {
+    char fileName[MFNL];
+    char token[MAX_TOKEN_LENGTH];
+    char line[MAX_LINES];
+    char folderName[MAX_FOLDER_LENGTH];
+    char cwd[MAX_FOLDER_NAME_LENGTH];
+    FILE * file;
+    int numFiles;
+    int isCreated;
+    int isDeleted;
+    int isDisplaying;
+} file_explorer;
+
+void add_file();
+void add_dir();
+void del();
+void displayFile(const char *fileName);
+void showThisDir();
+void goToDir(const char *path);
+void list();
+
+#endif
+
