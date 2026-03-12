6b555c0ab858920c9869a5df1ffdeab6cbba8528
deltadev-rsc <deltaqxq174@mail.ru>
2026-03-12 20:43:27 +0500

    Create simple_comms.h

---
+++ shell/src/lib/simple_comms.h
@@ -0,0 +1,32 @@
+/* help function */
+#ifndef SIMPLE_COMMANDS_HELP_H
+#define SIMPLE_COMMANDS_HELP_H
+
+void help();
+
+#endif
+
+/* system files */
+#ifndef SIMPLE_COMMANDS_FILES_H
+#define SIMPLE_COMMANDS_FILES_H
+
+void files();
+
+#endif
+
+/* big title OS */
+#ifndef TITLE_OS_H
+#define TITLE_OS_H
+
+void write_logo();
+
+#endif
+
+/* welcome label */
+#ifndef WELCOME_H
+#define WELCOME_H
+
+void welcome();
+
+#endif
+
