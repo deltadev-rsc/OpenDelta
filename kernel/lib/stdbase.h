c1d880928bf4bb0fcbe0e374ae723bd57701e282
deltadev-rsc <deltaqxq174@mail.ru>
2026-03-12 20:52:02 +0500

    Create stdbase.h

---
+++ kernel/lib/stdbase.h
@@ -0,0 +1,34 @@
+#ifndef BASE_H
+#define BASE_H
+
+#include "./types.h"
+
+#define ALIGN 8
+#define MAGIC 0xB16B00B5U
+
+/* colors */
+typedef enum {
+    WHITE = 0x0F,
+    BLACK = 0x0,
+    CYAN = 0x3,
+    DGRAY = 0x8,   // Dark Gray
+    LCYAN = 0xB,   // Light Cyan
+    LGRAY = 0x7    // Light Gray
+} Colors;
+
+typedef struct {
+    uint32_t magic;
+    size_t size;
+    int free;
+    struct block_header *prev;
+    struct block_header *next;
+} block_header_t;
+
+/* input-output */
+uint8_t inb(uint16_t port);
+void outb(uint16_t port, uint8_t val);
+void prints(const char *str, unsigned int color);
+void kprint(const char *str, unsigned int color);
+void kprintnl(void);
+
+#endif
