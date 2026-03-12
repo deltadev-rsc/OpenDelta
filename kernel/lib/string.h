be45151e36cabc124f262be6d8a833c49a09b853
deltadev-rsc <deltaqxq174@mail.ru>
2026-03-12 20:52:39 +0500

    Create string.h

---
+++ kernel/lib/string.h
@@ -0,0 +1,32 @@
+#include "types.h"
+
+#define size_t		unsigned long int
+
+void backspace(char s[]);
+
+void flush(char *var);
+
+void *memcpy(void *dest, const void *src, size_t n);
+void *memset(void *str, int c, size_t n);
+void memory_copy(unsigned char *source, unsigned char *dest, int nbytes);
+void memory_set(uint8_t *dest, uint8_t val, uint32_t len);
+
+int strcmp(const char *stra, const char *strb);
+int strncmp(const char *cs, const char *ct, size_t n); 
+
+size_t strlen(const char *str);
+size_t strnlen(const char *s, size_t maxlen);
+
+char *strcat(char *dest, const char *src);
+
+char *s_strncpy(char *s1, const char *s2, size_t n);
+
+void int_to_ascii(int n, char str[]);
+
+void hex_to_ascii(int n, char str[]);
+
+int s_sprintf(char *buf, const char *fmt);
+
+void *s_malloc(unsigned int size);
+
+uint32_t kmalloc(size_t size, int align, uint32_t *phys_addr);
