10db8292a1e31a886f0b6ee0ad1fba0b8ee898bc
deltadev-rsc <deltaqxq174@mail.ru>
2026-03-13 18:02:41 +0500

    Create entry_i386.asm

---
+++ kernel/entry_i386.asm
@@ -0,0 +1,31 @@
+[bits 32]
+
+global start
+global read_port
+global write_port
+global loadi ; load idt
+
+extern entry_point
+extern kmain
+
+read_port:
+    mov edx, [esp + 4]
+    in al, dx
+    ret
+
+write_port:
+    mov edx, [esp + 4]
+    mov al, [esp + 4 + 4]
+    out dx, al
+    ret
+
+loadi:
+    mov edx, [esp + 4]
+    lidt [edx]
+    sti
+    ret
+
+start:
+    call entry_point
+    call kmain
+    jmp $
