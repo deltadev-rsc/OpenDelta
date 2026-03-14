d92d377da9ee3e3caddeb3ec3aa9282e73555e6d
deltadev-rsc <deltaqxq174@mail.ru>
2026-03-14 21:03:50 +0500

    Update kernel.c

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -2,10 +2,22 @@
 #include "./lib/types.h"
 #include "./lib/multiboot.h"
 #include "./lib/string.h"
+#include "./lib/isr.h"
+#include "./lib/idt.h"
 
 extern char readp(uint16_t port);
 extern void writep(uint16_t port, uint8_t data);
 extern void load_idt(uintptr_t *idt_ptr);
+
+struct IDTEntry IDT[IDT_SIZE];
+struct multiboot *mboot = NULL;
+
+void idt_init(void) 
+{
+    unsigned long kbaddress;
+    unsigned long idt_address;
+    unsigned long idt_ptr[2];
+}
 
 void entry_point()
 {
