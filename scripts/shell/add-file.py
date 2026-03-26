a71a3033e5656f078abffcacedaa965120ccdc84
deltadev-rsc <deltaqxq174@mail.ru>
2026-03-26 10:06:47 +0500

    mini-update add-file.py

--- scripts/shell/add-file.py
+++ scripts/shell/add-file.py
@@ -13,7 +13,7 @@ T_MAGENTA = "\033[0;35m"
 def add(flag, name):
     if flag == "-f":
         try:
-            subprocess.run(["touch", name], check=True, capture_output=True, text=True)
+            subprocess.run(['touch'] + name, check=True, capture_output=True, text=True)
             print(T_GREEN, f"файл: {name} успешно создан", T_RESET)
         except subprocess.CalledProcessError as e:
             print(T_RED, "ошибка создания файла", T_RESET)
@@ -22,7 +22,7 @@ def add(flag, name):
 
     elif flag == "-d":
         try:
-            subprocess.run(["mkdir", name], check=True, capture_output=True, text=True)
+            subprocess.run(['mkdir'] + name, check=True, capture_output=True, text=True)
             print(T_GREEN, f"директория: {name} успешно создана", T_RESET)
         except subprocess.CalledProcessError as e:
             print(T_RED, "ошибка создания папки", T_RESET)
