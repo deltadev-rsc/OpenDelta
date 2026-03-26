c6278d9fa6363ad8eebbc8cf8920ce29e447a960
deltadev-rsc <deltaqxq174@mail.ru>
2026-03-26 09:50:16 +0500

    Update build.sh

--- kernel/boot/x86_64/build/build.sh
+++ kernel/boot/x86_64/build/build.sh
@@ -5,23 +5,23 @@ CC = clang
 CXX = clang++
 LD = ld.ldd
 
-$FLAGS = -m64 -nostdlib -ffreestanding -fno-pie  
-$LD_FLAGS = -m elf_x86_64 -z noexecstack -T ~/open-delta/kernel/boot/x86_64/linker.ld -Ttext 0x10000 --oformat binary
+CC_FLAGS = -m64 -nostdlib -ffreestanding -fno-pie  
+LD_FLAGS = -m elf_x86_64 -z noexecstack -T ~/open-delta/kernel/boot/x86_64/linker.ld -Ttext 0x10000 --oformat binary
 
 #---source-code-and-binaries---#
-bootPath = "~/open-delta/kernel/boot/x86_64/"
-bootBinPath = "~/open-delta/kernel/img/boot_x86_64.bin"
-kernEntryPath = "~/open-delta/kernel/kernel_entry.asm"
-kernEntryI386Path = "~/open-delta/kernel/entry_i386.asm"
-kernEntryBinPath = "~/open-delta/kernel/obj/kern_entry.o"
-kernEntryI386BinPath = "~/open-delta/kernel/obj/entry_i386.o"
+bootPath = "~/OpenDelta/kernel/boot/x86_64/boot.asm"
+bootBinPath = "~/OpenDelta/kernel/img/boot_x86_64.bin"
+kernEntryPath = "~/OpenDelta/kernel/kernel_entry.asm"
+kernEntryI386Path = "~/OpenDelta/kernel/entry_i386.asm"
+kernEntryBinPath = "~/OpenDelta/kernel/obj/kern_entry.o"
+kernEntryI386BinPath = "~/OpenDelta/kernel/obj/entry_i386.o"
 
 #---source-code-and-binaries-for-kernel---#
-kernelSourcePath = "~/open-delta/kernel/kernel.c"
-kernelObjPath = "~/open-delta/kernel/obj/kernel.o"
-kernelBinPath = "~/open-delta/kernel/img/kernel.bin"
-idtSourcePath = "~/open-delta/kernel/cpu/idt.c"
-idtBinPath = "~/open-delta/kernel/obj/idt.o"
+kernelSourcePath = "~/OpenDelta/kernel/kernel.c"
+kernelObjPath = "~/OpenDelta/kernel/obj/kernel.o"
+kernelBinPath = "~/OpenDelta/kernel/img/kernel.bin"
+idtSourcePath = "~/OpenDelta/kernel/cpu/idt.c"
+idtBinPath = "~/OpenDelta/kernel/obj/idt.o"
 
 #---function-for-build---#
 build() {
