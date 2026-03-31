89e83fd68aeaf108ade4426d4748b866ffc10cb3
deltadev-rsc <deltaqxq174@mail.ru>
2026-03-31 11:33:26 +0500

    Update boot.asm

--- kernel/boot/x86_64/boot.asm
+++ kernel/boot/x86_64/boot.asm
@@ -1,33 +1,34 @@
-.include "constants.asm"
+%include "boot/x86_64/constants.asm"
+%include "boot/x86_64/int15.asm"
+%include "boot/x86_64/switch64.asm"
 
-.code16
-.section .boot
-.global 	startup
-.global 	init
+[bits 16]
+section .boot
+    global 	startup
+    global 	init
 
 startup:
-	ljmp 0x0000, .flush_segments 	# Ensure segment registers are not modified by BIOS before use.
+	ljmp 0x0000, .flush_segments
 
 .flush_segments:
 	cld
-	mov sp, startup			# Prep the stack register to start and startup.
-	xor ax, ax			# Zero out ax, and use it to empty other segment registers.
+	mov sp, startup
+	xor ax, ax
 	mov ds, ax
 	mov es, ax
 	mov ss, ax
 
-	movb [disk], dl			# Write disk number to disk.
-	mov ax, 0x0241			# ah = read sector function, al = 65 (for 65 read disk sectors)
-	# If using more than ~33k of disk space, another driver is required to load all the data.
-	mov bx, 0x7E00			# es:bx = memory location to copy data into. es is already zeroed.
-	mov cx, 0x0002			# ch = 0x00 (track), cl = 0x02 (sector)
-	xor dh , dh			# dh = 0x00 (head), dl = drive number.
+	movb [disk], dl
+	mov ax, 0x0241
+	mov bx, 0x7E00
+	mov cx, 0x0002
+	xor dh , dh
 	int 0x13
 .check_cpuid:
 	pushfd
 	pop eax
 	mov ecx, eax
-	xor eax, (1 << 21)		# EFLAGS bit 21 indicates support for CPUID.
+	xor eax, (1 << 21)
 	push eax
 	popfd
 	pushfd
@@ -36,84 +37,35 @@ startup:
 	popfd
 	xor eax, ecx
 	jz .no_cpuid
-	
-	mov eax, 0x80000000		# Flag for long mode.
+
+	mov eax, 0x80000000
 	cpuid
-	cmp eax, 0x80000001		# Check status
+	cmp eax, 0x80000001
 	jz .no_longmode
-	
+
 	mov ax, 0x2401
 	int 0x15
 
 	mov ax, 0x03
 	int 0x10
 
-.int15:
-	mov eax, 0xE820			# Get memory map.	
-	xor ebx, ebx			# Clear ebx
-	mov ecx, 24			# Request 24 byte entries.
-	mov edx, 0x0534D4150		# Magic number
-	mov di, MEMORY_MAP_BUFFER	# Set di to second dword after end of page table.
-	movb [di + 20], 0x01		# Force valid ACPI 3.X entry
-	xor bp, bp			# Clear bp
-	int 0x15
-	jc .int15_failed
-	mov edx, 0x0534D4150		# Reset edx in case the interrupt changed edx.
-	cmp eax, edx			# If success, they are equal.
-	jne .int15_failed		
-	test ebx, ebx			# If ebx is zero, list is 1 entry long.
-	jz .int15_failed
-	jmp .int15_cont
-
-.int15_failed:
-	cli
-	hlt
-
-.int15_loop:
-	mov ax, 0xE820			# Fix ax from interrupt.
-	movb [di + 20], 0x01		# Force valid ACPI entry.
-	mov ecx, 24			# 24 bytes requested.
-	int 0x15
-	jc .int15_failed
-	mov edx, 0x0534D4150		# Reset edx in case the interrupt changed edx.
-
-.int15_cont:
-	jcxz .skip_entry
-	cmp cl, 20			# If entry is 24 bytes, it's a valid ACPI entry.
-	jbe .not_acpi
-	testb [di + 20], 0x01		# 0x01 = ignore data flag
-	jz .skip_entry
-
-.not_acpi:
-	mov ecx, [di + 8]		# Get lower uint32_t of memory region length
-	or exc, [di + 12]		# OR it with the upper half to test for zero.
-	jz .skip_entry
-	inc bp
-	add di, 24			# set the pointer to the next entry.
-.skip_entry:
-	test ebx, ebx			# If ebx is 0, list is complete.
-	jnz .int15_loop
-.int15_finished:
-	movw ax, [disk]
-	movw [MEMORY_MAP_ENTRY_COUNT], bp # Store the entry count at the beginning of the list.
-
 .switch_longmode:
 	mov di, PAGE_TABLE_BUFFER
 	push di
-	
+
 	mov cx, 0x1000
 	xor eax, eax
 	cld
 	rep stosd
 	pop di
-	
+
 	movw [di], PAGE_TABLE_BUFFER + 0x1003
 	movw [di + 0x1000], PAGE_TABLE_BUFFER + 0x2003
 	movw [di + 0x2000], PAGE_TABLE_BUFFER + 0x3003
 	mov di, PAGE_TABLE_BUFFER, 0x3000
 	mov ax, 0x03
 .pages_loop:
-	mov [di], eax			# Identity map pages
+	mov [di], eax
 	add eax, 0x100
 	add di, 8
 	cmp di, PAGE_TABLE_BUFFER + 0x4000
@@ -121,35 +73,35 @@ startup:
 
 	mov al, 0xFF
 	out 0x21, al
-	out 0xA1, al			# Disable IRQs
-	
-	lidt [dummy_IDT]		# Load dummy IDT
-	
+	out 0xA1, al
+
+	lidt [dummy_IDT]
+
 	mov eax, 0xb10100000
-	mov cr4, eax			# Enable paging
-	
+	mov cr4, eax
+
 	mov edi, PAGE_TABLE_BUFFER
-	mov cr3, edi			# Copy table pointer into the control reg.
-	
+	mov cr3, edi
+
 	mov ecx, EFER
 	rdmsr
 	or eax, 0x00000101
 	wrmsr
 
-	mov ebx, cr0			
-	or ebx, 0x800000001		
-	mov cr0, ebx			# Enable protected mode.
+	mov ebx, cr0
+	or ebx, 0x800000001
+	mov cr0, ebx
 
 	mov ecx, LSTAR
 	mov eax, syscall_handler
 	wrmsr
-	
-	mov ecx, SFMASK	
+
+	mov ecx, SFMASK
 	mov eax, 0xFFFFFFFF
 	wrmsr
-	
-	lgdt [GDT_PTR]			# Load the GDT
-	ljmp KERNEL_CODE_SEGMET, 0x7E00	# Jump to long mode segment.
+
+	lgdt [GDT_PTR]
+	ljmp KERNEL_CODE_SEGMET, 0x7E00
 
 .no_cpuid:
 	mov si, no_cpuid_str
@@ -175,36 +127,38 @@ dummy_IDT:
 	.long 0x00
 disk: .byte 0x00
 .space 510 - (. - startup), 0
-.word 0xAA55
+dword 0xAA55
 
-.code64
+[bits 64]
 long_mode:
 	mov ax, KERNEL_DATA_SEGMENT
 	mov, ds, ax
 	mov es, ax
 	mov fs, ax
 	mov gs, ax
-	mov ss, ax			# Set selector registers.
-	
+	mov ss, ax
+
 	cli
 
 	xor rax, rax
 	xor rbx, rbx
 	xor rcx, rcx
 	xor rdx, rdx
-	xor rdi, rdi			# Zero out registers
+	xor rdi, rdi
 
 	mov rsp, KERNEL_STACK
-	mov rbp, rsp			# The stack is the top of kernel memory.
+	mov rbp, rsp
 
-	call kernel_main
+	call kmain
 
 	cli
 	hlt
+
 syscall_handler:
 	systetq
+
 GDT:
-.null: 
+.null:
 	.quad 0x00
 .kcode:
 	.quad 0x00209A0000000000
@@ -219,5 +173,5 @@ GDT_PTR:
 	.word GDT_END - GDT - 1
 	.long GDT
 
-no_cpuid_str: .ascii "Error: FATAL: CPU does not support CPUID."
-no_longmode_str: .ascii "Error: FATAL: CPU does not support long mode."
+no_cpuid_str: dq "[ERR FATAL]: [CPU does not support CPUID.]"
+no_longmode_str: dq "[ERR FATAL]: [CPU does not support long mode.]"
