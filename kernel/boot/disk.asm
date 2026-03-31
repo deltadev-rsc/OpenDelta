3ac56d449cd8b105cde8c585debf55a3aab71aae
deltadev-rsc <deltaqxq174@mail.ru>
2026-03-31 17:03:23 +0500

    Update disk.asm

--- kernel/boot/disk.asm
+++ kernel/boot/disk.asm
@@ -1,6 +1,31 @@
+section .data
+    error_msg: 
+        db "[err]: disk read error", 0x0D, 0x0A, 0x0
+        db "[err]: floppy read error", 0x0D, 0x0A, 0x0
+        
+    have_extensions:        db 0
+    extensions_dap:
+        .size:              db 10h
+                            db 0
+        .count:             dw 0
+        .offset:            dw 0
+        .segment:           dw 0
+        .lba:               dq 0
+
+    STAGE2_LOAD_SEGMENT     equ 0x0
+    STAGE2_LOAD_OFFSET      equ 0x500
+
+    PARTITION_ENTRY_SEGMENT equ 0x2000
+    PARTITION_ENTRY_OFFSET  equ 0x0
+
 disk_load:
     pusha
+    push eax 
+    push bx
+    push cx
     push dx
+    push si 
+    push di
 
     mov ah, 0x02
     mov al, dh 
@@ -9,14 +34,23 @@ disk_load:
     mov dh, 0x00 
     mov cl, 0x02
 
-    int 13h
+    int 0x13
     jc disk_error
 
     pop dx
     cmp al, dh
     jne sectors_error
 
+    call disk_done
+
+disk_done:
     popa 
+    pop di
+    pop si 
+    pop dx
+    pop cx
+    pop dx 
+    pop eax
     ret
 
 disk_error:
@@ -31,6 +65,4 @@ sectors_error:
     mov bx, error_msg
     call print
 
-error_msg: 
-    db "[err]: disk read error", 0x0D, 0x0A, 0x0
-    db "[err]: floppy read error", 0x0D, 0x0A, 0x0
+stage2_location: times 30 db 0
