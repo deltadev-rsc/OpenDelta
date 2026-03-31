917d7c5adefdfa7a7033eafc50ddbac03666959e
deltadev-rsc <deltaqxq174@mail.ru>
2026-03-31 21:20:50 +0500

    Create pipe.h

---
+++ kernel/fs/headers/pipe.h
@@ -0,0 +1,28 @@
+#ifndef _DLTFS_PIPE_H
+#define _DLTFS_PIPE_H
+
+#include "./fs.h"
+#include "./list.h"
+
+typedef struct _pipe_device {
+	uint8_t *buffer;
+	size_t write_ptr;
+	size_t read_ptr;
+	size_t size;
+	size_t refcount;
+	volatile int lock_read[2];
+	volatile int lock_write[2];
+	list_t *wait_queue_readers;
+	list_t *wait_queue_writers;
+	int dead;
+	list_t *alert_waits;
+} pipe_device_t;
+
+typedef struct {
+    list_t *node_waits;
+} process_t;
+
+fs_node_t *makePipe(size_t size);
+int pipeSize(fs_node_t *node);
+
+#endif
