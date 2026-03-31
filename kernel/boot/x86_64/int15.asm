e151f36ac95df9268e7cf06498a904c519d163dc
deltadev-rsc <deltaqxq174@mail.ru>
2026-03-31 11:33:52 +0500

    Create int15.asm

---
+++ kernel/boot/x86_64/int15.asm
@@ -0,0 +1,48 @@
+.int15:
+	mov eax, 0xE820
+	xor ebx, ebx
+	mov ecx, 24
+	mov edx, 0x0534D4150
+	mov di, MEMORY_MAP_BUFFER
+	movb [di + 20], 0x01
+	xor bp, bp
+	int 0x15
+	jc .int15_failed
+	mov edx, 0x0534D4150
+	cmp eax, edx
+	jne .int15_failed
+	test ebx, ebx
+	jz .int15_failed
+	jmp .int15_cont
+
+.int15_failed:
+	cli
+	hlt
+
+.int15_loop:
+	mov ax, 0xE820
+	movb [di + 20], 0x01
+	mov ecx, 24
+	int 0x15
+	jc .int15_failed
+	mov edx, 0x0534D4150
+
+.int15_cont:
+	jcxz .skip_entry
+	cmp cl, 20
+	jbe .not_acpi
+	testb [di + 20], 0x01
+	jz .skip_entry
+
+.not_acpi:
+	mov ecx, [di + 8]
+	or exc, [di + 12]
+	jz .skip_entry
+	inc bp
+	add di, 24
+.skip_entry:
+	test ebx, ebx
+	jnz .int15_loop
+.int15_finished:
+	movw ax, [disk]
+	movw [MEMORY_MAP_ENTRY_COUNT], bp
