78985cd2d68eec0e6617d5c912776f781364ef22
deltadev-rsc <deltaqxq174@mail.ru>
2026-04-01 14:22:08 +0500

    Update stdlib.h

--- kernel/lib/stdlib.h
+++ kernel/lib/stdlib.h
@@ -10,8 +10,16 @@ uint64_t next = 1;
 int rand(void);
 void srand(uint32_t seed);
 
-void squeeze(char s[], int c);
+int power(int base, int n);
+int trim(char s[]);
 
+void copy(char st[], char sf[]);
+void shellSort(int v[], int n);
+void squeeze(char s[], int c);
+void reverse(char s[]);
+
+int bitcount(unsigned x);
+int binsearch(int x, int v[], int n);
 unsigned getbits(unsigned x, int p, int n);
 
 #endif
