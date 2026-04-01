fce802d1a21c862ee5adb8d5ad8629d2c04ac32a
deltadev-rsc <deltaqxq174@mail.ru>
2026-04-01 14:18:23 +0500

    Update stdlib.c

--- kernel/lib/source/stdlib.c
+++ kernel/lib/source/stdlib.c
@@ -1,4 +1,5 @@
 #include "../types.h"
+#include "../string.h"
 
 uint64_t next = 1;
 
@@ -6,6 +7,103 @@ int rand(void)
 {
     next = next * 1103515245 + 12345;
     return (uint32_t) (next / 65536) % 32768;
+}
+
+int trim(char s[])
+{
+    int n;
+
+    for (n = strlen(s) - 1; n >= 0; n--) {
+        if (s[n] != ' ' && s[n] != '\t' && s[n] != '\n') {
+            break;
+        }
+    }
+
+    s[n + 1] = '\0';
+    return n;
+}
+
+int power(int base, int n)
+{
+    int i, p;
+
+    p = 1;
+    for (i = 1; i <= n; ++i) {
+        p = p * base;
+    }
+    return p;
+}
+
+int bitcount(unsigned x) 
+{
+    int b;
+
+    for (b = 0; x != 0; x >>= 1) {
+        if (x & 01) {
+            b++;
+        }
+    }
+
+    return b;
+}
+
+int binsearch(int x, int v[], int n)
+{
+    int low, high, mid;
+
+    low = 0;
+    high = n - 1;
+
+    while (low <= high) {
+        mid = (low + high) / 2;
+        if (x < v[mid]) {
+            high = mid - 1;
+        }
+        else if (x > v[mid]) {
+            low = mid + 1;
+        }
+        else {
+            return mid;
+        }
+    }
+
+    return -1;
+}
+
+void copy(char st[], char sf[])
+{
+    int i;
+
+    i = 0;
+    while ((st[i] = sf[i]) != '\0') {
+        ++i;
+    }
+}
+
+void shellSort(int v[], int n)
+{
+    int gap, i, j, temp;
+
+    for (gap = n/2; gap > 0; gap /= 2) {
+        for (i = gap; i < n; i++) {
+            for (j=i - gap; j >= 0 && v[j] > v[j + gap]; j -= gap) {
+                temp = v[j];
+                v[j] = v[j + gap];
+                v[j + gap] = temp;
+            }
+        }
+    }
+}
+
+void reverse(char s[])
+{
+    int c, i, j;
+
+    for (i = 0, j = strlen(s) - 1; i < j; i++, j--) {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
 }
 
 void srand(uint32_t seed) 
@@ -27,6 +125,7 @@ void squeeze(char s[], int c)
     s[j] = '\0';
 }
 
-unsigned getbits(unsigned x, int p, int n) {
+unsigned getbits(unsigned x, int p, int n) 
+{
     return (x >> (p+1-n)) & ~ (~0 << n);
 } 
