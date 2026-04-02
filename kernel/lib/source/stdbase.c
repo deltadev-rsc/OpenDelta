d7009f01ba4d32f498f0cfad1459ac8c74d55fd1
deltadev-rsc <deltaqxq174@mail.ru>
2026-04-02 11:53:59 +0500

    Update stdbase.c

--- kernel/lib/source/stdbase.c
+++ kernel/lib/source/stdbase.c
@@ -2,6 +2,93 @@
 #include "../types.h"
 #include "../string.h"
 
+int _fillbuf(FILE *);
+int _flushbuf(int, FILE *);
+extern FILE _iob[OPEN_MAX];
+
+static Header base;
+static Header *freep = NULL;
+
+static Header *morecore(unsigned nu)
+{
+    char *cp, *sbrk(int);
+    Header *up;
+
+    if (nu < NALLOC) {
+        nu = NALLOC;
+    }
+
+    cp = sbrk(nu * sizeof(Header));
+    if (cp == (char *) - 1) {
+        return NULL;
+    }
+    up = (Header *) cp;
+    up->s.size = nu;
+    free((void *) (up + 1));
+    return freep;
+}
+
+void *free(void *ap)
+{
+    Header *bp, *p;
+    for (p = freep; !(bp > p && bp < p->s.ptr); p = p->s.ptr) {
+        if (p >= p->s.ptr && (bp > p || bp < p->s.ptr)) {
+            break;
+        }
+    }
+
+    if (bp + bp->s.size == p->s.ptr) {
+        bp->s.size += p->s.ptr->s.size;
+        bp->s.ptr = p->s.ptr->s.ptr;
+    }
+    else {
+        bp->s.ptr = p->s.ptr;
+    }
+
+    if (p + p->s.size == bp) {
+        p->s.size += bp->s.size;
+        p->s.ptr = bp->s.ptr;
+    }
+    else {
+        p->s.ptr = bp;
+    }
+
+    freep = p;
+}
+
+void *malloc(unsigned nbytes)
+{
+    Header *p, *prevp;
+    unsigned nunits;
+
+    nunits = (nbytes + sizeof(Header) - 1) / sizeof(Header) + 1;
+    if ((prevp = freep) == NULL) {
+        base.s.ptr = freep = prevp = &base;
+        base.s.size = 0;
+    }
+    
+    for (p = prevp->s.ptr; ; prevp = p, p = p->s.ptr) {
+        if (p->s.size >= nunits) {
+            if (p->s.size == nunits) {
+                prevp->s.ptr = p->s.ptr;
+            }
+            else {
+                p->s.size -= nunits;
+                p += p->s.size;
+                p->s.size = nunits;
+            }
+            freep = prevp;
+            return (void *)(p + 1);
+        }
+
+        if (p == freep) {
+            if ((p = morecore(nunits)) == NULL) {
+                return NULL;
+            }
+        }
+    }
+}
+
 void outb(uint16_t port, uint8_t val) {
     __asm__ __volatile__ ("outb %0, %1" : : "a" (val), "Nd" (port));
 }
