357444fea51c37a031c449b6c7b2f84a946d41a6
deltadev-rsc <deltaqxq174@mail.ru>
2026-04-03 19:53:44 +0500

    Update gdt.asm

--- kernel/boot/gdt.asm
+++ kernel/boot/gdt.asm
@@ -1,8 +1,10 @@
+global load_gdt
+
 gdt_start:
 
 gdt_null:
-    dd 0x0
-    dd 0x0
+    dq 0x0
+    dq 0x0
 
 gdt_code:
     dw 0xffff
@@ -29,3 +31,7 @@ gdt_descriptor:
 CODE_SEG equ gdt_code - gdt_start
 DATA_SEG equ gdt_data - gdt_start
 
+load_gdt:
+    [bits 16]
+    lgdt [gdt_descriptor]
+    ret
