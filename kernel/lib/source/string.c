3a11a95eac6f6550fcf68acbf968aaf0eeaea2fe
deltadev-rsc <deltaqxq174@mail.ru>
2026-04-03 21:11:54 +0500

    Update string.c

--- kernel/lib/source/string.c
+++ kernel/lib/source/string.c
@@ -1,65 +1,5 @@
 #include "../string.h"
-#include "../types.h"
-
-void append(char s[], char n)
-{
-	int len = strlen(s);
-	s[len] = n;
-	s[len+1] = '\0';
-}
-
-void backspace(char s[])
-{
-	int len = strlen(s);
-	s[len-1] = '\0';
-}
-
-void flush(char *var) 
-{
-	int i = 0;
-	while(var[i] != '\0')  {
-		var[i] = '\0';
-		i++;
-	}
-}
-
-void *memcpy(void *dest, const void *src, size_t n)
-{
-	char *d = dest;
-	const char *s = src;
-	
-	for(size_t i = 0; i < n; i++) {
-		d[i] = s[i];
-	}
-	return dest;
-}
-
-void *memset(void *str, int c, size_t n)
-{
-	char *s = str;
-
-	for(size_t i = 0; i < n; i++) {
-		*(s + i) = c;
-	}
-
-	return str;
-}
-
-void memory_copy(unsigned char *source, unsigned char *dest, int nbytes) 
-{
-	int i;
-	for (i = 0; i < nbytes; i++)  {
-		*(dest + i) = *(source + i);
-	}
-}
-
-void memory_set(uint8_t *dest, uint8_t val, uint32_t len)
-{
-	uint8_t *temp = (uint8_t *)dest;
-	for( ; len != 0; len--) {
-		 *temp++ = val;
-	}
-}
+#include "../../mem/header/memory.h"
 
 int strindex(char s[], char t[]) 
 {
@@ -210,7 +150,7 @@ size_t s_vasprintf(char *buf, const char *fmt)//, va_list args)
 	return 1;
 }
 
-int s_sprintf(char *buf, const char *fmt)
+int sprintf(char *buf, const char *fmt)
 {
 	int out = s_vasprintf(buf, fmt);//, args);
 	return out;
@@ -240,4 +180,5 @@ uint32_t kmalloc(size_t size, int align, uint32_t *phys_addr)
 	free_mem_addr += size; 
 	return ret;
 }
+
 
