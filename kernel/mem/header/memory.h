5626500ed249a719982276491009f1f05aa90fc2
deltadev-rsc <deltaqxq174@mail.ru>
2026-04-04 19:50:51 +0500

    Update memory.h

--- kernel/mem/header/memory.h
+++ kernel/mem/header/memory.h
@@ -2,6 +2,7 @@
 #define MEMORY_H
 
 #include "../../lib/types.h"
+#include "../../lib/system.h"
 
 void append(char s[], char n);
 void backspace(char s[]);
@@ -11,5 +12,9 @@ void *memcpy(void *dest, const void *src, size_t n);
 void *memset(void *str, int c, size_t n);
 void memmory_copy(unsigned char *source, unsigned char *dest, int nbytes);
 void memory_set(uint8_t *dest, uint8_t val, uint32_t len);
+
+void paggingInstall(uint32_t memsize);
+void heapInstall(void);
+void switchPageDir(page_dir_t *dir);
 
 #endif 
