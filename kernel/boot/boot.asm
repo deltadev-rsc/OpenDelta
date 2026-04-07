3cd41e656f24ae699b0baf174e9bbf47829c0388
deltadev-rsc <deltaqxq174@mail.ru>
2026-04-07 17:21:41 +0500

    Update boot.asm

--- kernel/boot/boot.asm
+++ kernel/boot/boot.asm
@@ -1,5 +1,5 @@
 [bits 16]
-[org 0x7c00]
+[org 0x7C00]
 
 ; includes like .h in C
 %include "boot/i386/disk.asm"
@@ -21,7 +21,9 @@ section .data
     str_load: dw "Loading dltkernel from the disk", ENDL, 0
     
     boot_drive db 0x0
- 
+    boot_part_seg dw 0
+    boot_part_off dw 0
+    
     ; errors
     str_returned_kernel: dw "Returned from kernel. Error?", 0
     str_read_fail: dw "[err]: [read failed!]", 0
@@ -32,6 +34,8 @@ section .text
     global _start
 
 _start:
+    cli 
+
     mov ax, PARTITION_ENTRY_SEGMENT
     mov es, ax
     mov di, PARTITION_ENTRY_OFFSET
@@ -43,17 +47,17 @@ _start:
     mov es, ax
 
     mov ss, ax
-    mov sp, 0x7c00 
+    mov sp, 0x7C00 
 
     mov bx, 0x100000
     mov es, bx
     int 13h
 
-
     mov  bx, str_real 
     call print
     call print_nl
 
+    call load_gdt
     call load_kernel
     call switch
 
@@ -75,12 +79,26 @@ load_kernel:
 [bits 32]
 begin_pm:
     mov ebx, str_pmode
+    mov ax, 0x10 
     mov bx, 0x100000
     mov ds, ax
     mov ss, ax
     mov es, ax
     mov fs, ax
     mov gs, ax
+
+    mov dx, [boot_part_seg]
+    shl edx, 16
+    mov dx, [boot_part_off]
+    push edx 
+
+    xor edx, edx
+    mov dl, [boot_drive]
+    push edx
+    call _start 
+
+    cld
+    rep stosb
 
     call print_pmode
     call kernel_offset
