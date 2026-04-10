ec07a6a3f7958f3fc822a3860cbd287516d9bcbe
deltadev-rsc <deltaqxq174@mail.ru>
2026-04-10 22:35:23 +0500

    Update dexide.rs

--- shell/src/dexide.rs
+++ shell/src/dexide.rs
@@ -1,14 +1,14 @@
 /*
-  переписываю древний редактор dex из dltsh на rust
+  переписываю dexide из dltsh с си на растик.
 */
-use std::io;
 use std::fs::File;
-use std::io::Read;
+use std::io::{BufRead, BufReader};
+use std::io;
 
 const MAX_LINES: usize = 512;
 const MAX_LINE_LENGTH: usize = 1024;
 
-pub fn EditorLogo() {
+pub fn editor_logo() {
     println!("████████        ███████     █       █   ██  ████████        ███████    ");
     println!("██     ██      ██     ██    ██     ██       ██     ██      ██     ██   ");
     println!("██      ██    ██       ██    ██   ██    ██  ██      ██    ██       ██  ");
@@ -18,20 +18,31 @@ pub fn EditorLogo() {
     println!("█████████      ██████████   █       █   ██  █████████      ██████████  ");
 }
 
-pub fn main() {
+pub fn main() -> io::Result<()> {
     println!("welcome to Dexide!");
-    EditorLogo();
+    editor_logo();
 
-    let lines: [[char; MAX_LINE_LENGTH]; MAX_LINES];
-    let mut fileName: &str;
+    let mut lines: Vec<String> = Vec::new(); //: [[char; MAX_LINE_LENGTH]; MAX_LINES];
+    let mut file_name = String::new();
     let mode: char;
-    let lineCount: i64;
+    let mut line_count: usize = 0;
 
     println!("input file name to edit: ");
-    
     io::stdin()
-        .read_line(&mut fileName)
+        .read_line(&mut file_name)
         .expect("[err]: [failed to read file name]");
-    fileName = fileName.trim().to_string();
-    let mut file = File::create(fileName);
+    file_name = file_name.trim().to_string();
+
+    let file = File::open(&file_name)?;
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        if line_count >= MAX_LINES {
+            break;
+        }
+        lines.push(line?);
+        line_count += 1;
+    }
+
+    Ok(())
 }
