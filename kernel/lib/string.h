ed86399653fa85bc1650d16f872b9a4ca5d84047
deltadev-rsc <deltaqxq174@mail.ru>
2026-04-10 21:54:22 +0500

    Update string.h

--- kernel/lib/string.h
+++ kernel/lib/string.h
@@ -15,14 +15,12 @@ size_t strnlen(const char *s, size_t maxlen);
 
 char *strcat(char *dest, const char *src);
 
-char *s_strncpy(char *s1, const char *s2, size_t n);
+char *strncpy(char *s1, const char *s2, size_t n);
 
 void int_to_ascii(int n, char str[]);
 
 void hex_to_ascii(int n, char str[]);
 
 int sprintf(char *buf, const char *fmt);
-
-void *s_malloc(unsigned int size);
 
 #endif
