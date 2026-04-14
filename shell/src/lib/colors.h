e6e6a58f867a3bb4696ef706e3e475c224ba0538
deltadev-rsc <deltaqxq174@mail.ru>
2026-04-14 13:23:01 +0500

    Create colors.h

---
+++ shell/src/lib/colors.h
@@ -0,0 +1,20 @@
+#ifndef COLORS_H
+#define COLORS_H
+
+#define T_RED     "\x1b[31m"
+#define T_GREEN   "\x1b[32m"
+#define T_BLUE    "\x1b[34m"
+#define T_YELLOW  "\x1b[33m"
+#define T_CYAN    "\x1b[36m"
+#define T_RESET   "\x1b[0m"
+#define T_MAGENTA "\x1b[35m"
+
+#define ASCII_RED "\033[38;2;255;0;0m"
+#define ASCII_GREEN "\033[38;2;0;255;0m"
+#define ASCII_BLUE "\033[38;2;0;0;255m"
+#define ASCII_YELLOW "\033[38;2;255;255;0m"
+#define ASCII_CYAN "\033[38;2;0;255;255m"
+#define ASCII_RESET "\033[0m"
+#define ASCII_MAGENTA "\033[0;35m"
+
+#endif 
