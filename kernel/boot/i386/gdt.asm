9fe31d4a9066aab941aa3ca1c3591d7efe086eba
deltadev-rsc <deltaqxq174@mail.ru>
2026-04-16 21:00:02 +0500

    Update boot/gdt.asm and move boot/gdt.asm to boot/i386/

--- kernel/boot/i386/gdt.asm
+++ kernel/boot/i386/gdt.asm
@@ -1,25 +1,24 @@
-global load_gdt
+org 0x7C00
 
 gdt_start:
 
 gdt_null:
-    dq 0x0
     dq 0x0
 
 gdt_code:
     dw 0xFFFF
     dw 0x0
     db 0x0
-    db 10011010b
-    db 11001111b
+    db 0x9A
+    db 0xCF
     db 0x0
 
 gdt_data:
     dw 0xFFFF 
     dw 0x0
     db 0x0
-    db 10010010b
-    db 11001111b
+    db 0x92
+    db 0xCF
     db 0x0
 
 gdt_end:
@@ -30,8 +29,3 @@ gdt_descriptor:
 
 CODE_SEG equ gdt_code - gdt_start
 DATA_SEG equ gdt_data - gdt_start
-
-load_gdt:
-    [bits 16]
-    lgdt [gdt_descriptor]
-    ret
