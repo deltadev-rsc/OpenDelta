dadc89dcc50ff560d6e8f70313a5f38ebc68885c
deltadev-rsc <deltaqxq174@mail.ru>
2026-04-16 21:31:25 +0500

    Update build-shell.sh

--- scripts/shell/build-shell.sh
+++ scripts/shell/build-shell.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 function build() {
-    cd ~/open-delta/kernel/shell/
+    cd ~/OpenDelta/shell/
     clang src/term.c src/files.c src/dltsh.c src/commands.c src/simple_comms.c -o bin/dltsh
-    rustc src/dexide.rs -o bin/dexide
+    clang++ src/help_table.cpp -o bin/table -lncursesw
+	rustc src/dexide.rs -o bin/dexide
     rustc src/calc.rs -o bin/calc
     rustc src/clocks.rs -o bin/clock
 }
