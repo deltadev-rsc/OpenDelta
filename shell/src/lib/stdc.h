b3969a87a99e9e61f33737f914c30ba1637c899a
deltadev-rsc <deltaqxq174@mail.ru>
2026-04-18 22:10:28 +0500

    Create stdc.h

---
+++ shell/src/lib/stdc.h
@@ -0,0 +1,59 @@
+#ifndef _STDC_H
+#define _STDC_H
+
+#if !defined (__P) 
+    #if defined (__STDC__) || defined (__GNUC__) || defined (__cplusplus) || defined (PROTOTYPES)
+        #define __P(protos) protos
+    #else
+        #define __P(protos) ()
+    #endif
+#endif
+
+#if defined (HAVE_STRINGIZE)
+    #define CPP_STRING(x) #x
+#else 
+    #define CPP_STRING(x) "x"
+#endif
+
+#if !defined (__STDC__) 
+    
+    #if defined (__GNUC__)
+        #if !defined (signed)
+            #define signed __signed
+        #endif
+        #if !defined (volatile)
+            #define volatile __volatile
+        #endif
+    #else
+        #if !defined (inline)
+            #define inline
+        #endif
+        #if !defined (signed)
+            #define signed
+        #endif
+        #if !defined (volatile)
+            #define volatile
+        #endif
+    #endif
+
+#endif
+
+#ifndef __attribute__
+    #if __GNUC__ < 2 || (__GNUC__ == 2 && __GNUC_MINOR__ < 8)
+        #define __attribute(x)
+    #endif 
+#endif
+
+#ifdef __GNUC__
+    #define INLINE inline
+#else
+    #define INLINE
+#endif
+
+#if defined (PREFER_STDARG)
+    #define SH_VA_START(va, arg)  va_start(va, arg)
+#else
+    #define SH_VA_START(va, arg)  va_start(va)
+#endif 
+
+#endif 
