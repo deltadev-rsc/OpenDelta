ff330de23ce2f4d2ffc054d339e90ddb3cca1960
deltadev-rsc <deltaqxq174@mail.ru>
2026-04-20 09:18:37 +0500

    Update boot.asm

--- kernel/boot/x86_64/boot.asm
+++ kernel/boot/x86_64/boot.asm
@@ -1,14 +1,21 @@
+[bits 16]
+[org 0x7C00]
+
 %include "boot/x86_64/constants.asm"
 %include "boot/x86_64/int15.asm"
 %include "boot/x86_64/switch64.asm"
 
-[bits 16]
+section .data
+	no_cpuid_str: dq "[ERR FATAL]: [CPU does not support CPUID.]"
+	no_longmode_str: dq "[ERR FATAL]: [CPU does not support long mode.]"
+
+
 section .boot
     global 	startup
     global 	init
 
 startup:
-	ljmp 0x0000, .flush_segments
+	ljmp 0x10000, .flush_segments
 
 .flush_segments:
 	cld
@@ -49,60 +56,6 @@ startup:
 	mov ax, 0x03
 	int 0x10
 
-.switch_longmode:
-	mov di, PAGE_TABLE_BUFFER
-	push di
-
-	mov cx, 0x1000
-	xor eax, eax
-	cld
-	rep stosd
-	pop di
-
-	movw [di], PAGE_TABLE_BUFFER + 0x1003
-	movw [di + 0x1000], PAGE_TABLE_BUFFER + 0x2003
-	movw [di + 0x2000], PAGE_TABLE_BUFFER + 0x3003
-	mov di, PAGE_TABLE_BUFFER, 0x3000
-	mov ax, 0x03
-.pages_loop:
-	mov [di], eax
-	add eax, 0x100
-	add di, 8
-	cmp di, PAGE_TABLE_BUFFER + 0x4000
-	jl .pages_loop
-
-	mov al, 0xFF
-	out 0x21, al
-	out 0xA1, al
-
-	lidt [dummy_IDT]
-
-	mov eax, 0xb10100000
-	mov cr4, eax
-
-	mov edi, PAGE_TABLE_BUFFER
-	mov cr3, edi
-
-	mov ecx, EFER
-	rdmsr
-	or eax, 0x00000101
-	wrmsr
-
-	mov ebx, cr0
-	or ebx, 0x800000001
-	mov cr0, ebx
-
-	mov ecx, LSTAR
-	mov eax, syscall_handler
-	wrmsr
-
-	mov ecx, SFMASK
-	mov eax, 0xFFFFFFFF
-	wrmsr
-
-	lgdt [GDT_PTR]
-	ljmp KERNEL_CODE_SEGMET, 0x7E00
-
 .no_cpuid:
 	mov si, no_cpuid_str
 	call printstr_rmode
@@ -122,9 +75,11 @@ startup:
 	jmp printstr_rmode
 .done:
 	ret
+
 dummy_IDT:
 	.word 0x00
 	.long 0x00
+
 disk: .byte 0x00
 .space 510 - (. - startup), 0
 dword 0xAA55
@@ -171,7 +126,4 @@ GDT:
 GDT_END:
 GDT_PTR:
 	.word GDT_END - GDT - 1
-	.long GDT
-
-no_cpuid_str: dq "[ERR FATAL]: [CPU does not support CPUID.]"
-no_longmode_str: dq "[ERR FATAL]: [CPU does not support long mode.]"
+	.qword GDT
