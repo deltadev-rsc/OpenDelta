1d5e4bb08f5b9f3c2c4134e3b1fc022e2d6ad152
deltadev-rsc <deltaqxq174@mail.ru>
2026-04-23 20:06:09 +0500

    Create gdt.h

---
+++ kernel/gdt/gdt.h
@@ -0,0 +1,72 @@
+#ifndef GDT_H
+#define GDT_H
+
+#pragma once
+
+#include "../lib/types.h"
+
+#define i386_GDT_CODE_SEG 0x08
+#define i386_GDT_DATA_SEG 0x01
+
+typedef struct {
+    uint16_t limit_low;
+    uint16_t base_low;
+    uint8_t base_middle;
+    uint8_t access;
+    uint8_t flags_limit_high;
+    uint8_t base_high;
+} __attribute__((packed)) GDTEntry;
+
+typedef struct {
+    uint16_t limit;
+    GDTEntry* ptr;
+} __attribute__((packed)) GDTDescriptor;
+
+typedef enum {
+    GDT_ACCESS_CODE_READABLE                = 0x02,
+    GDT_ACCESS_DATA_WRITEABLE               = 0x02,
+
+    GDT_ACCESS_CODE_CONFORMING              = 0x04,
+    GDT_ACCESS_DATA_DIRECTION_NORMAL        = 0x00,
+    GDT_ACCESS_DATA_DIRECTION_DOWN          = 0x04,
+
+    GDT_ACCESS_DATA_SEGMENT                 = 0x10,
+    GDT_ACCESS_CODE_SEGMENT                 = 0x18,
+
+    GDT_ACCESS_DESCRIPTOR_TSS               = 0x00,
+
+    GDT_ACCESS_RING0                        = 0x00,
+    GDT_ACCESS_RING1                        = 0x20,
+    GDT_ACCESS_RING2                        = 0x40,
+    GDT_ACCESS_RING3                        = 0x60,
+
+    GDT_ACCESS_PRESENT                      = 0x80
+} GDT_ACCESS;
+
+typedef enum {
+    GDT_FLAGS_64BIT =    0x20,
+    GDT_FLAG_32BIT  =    0x40,
+    GDT_FLAG_16BIT  =    0x00,
+
+    GDT_FLAG_GRANULARITY_1B = 0x00,
+    GDT_FLAG_GRANULARITY_4K = 0x80,
+} GDT_FLAGS;
+
+#define GDT_LIMIT_LOW(limit)                (limit & 0xFFFF)
+#define GDT_BASE_LOW(base)                  (base & 0xFFFF)
+#define GDT_BASE_MIDDLE(base)               ((base >> 16) & 0xFF)
+#define GDT_FLAGS_LIMIT_HIGH(limit, flags)    (((limit >> 16) & 0xF) | (flags & 0xF0))
+#define GDT_BASE_HIGH(base)                 ((base >> 24) & 0xFF)
+
+#define GDT_ENTRY(base, limit, access, flags) {                     \
+    GDT_LIMIT_LOW(limit),                                           \
+    GDT_BASE_LOW(base),                                             \
+    GDT_BASE_MIDDLE(base),                                          \
+    access,                                                         \
+    GDT_FLAGS_LIMIT_HIGH(limit, flags),                               \
+    GDT_BASE_HIGH(base)                                             \
+}
+void i386_GDT_load(GDTDescriptor *descriptor,uint16_t code_seg, uint16_t data_seg );
+void i386_GDT_init();
+
+#endif 
