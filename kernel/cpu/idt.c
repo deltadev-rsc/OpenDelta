9726371c3c81152bff30224bacc9e589e0777471
deltadev-rsc <deltaqxq174@mail.ru>
2026-04-24 17:10:20 +0500

    Update idt.c

--- kernel/cpu/idt.c
+++ kernel/cpu/idt.c
@@ -3,8 +3,15 @@
 idt_gate_t idt[IDT_ENTRIES];
 idt_reg_t idt_reg;
 
-void set_idt_gate(int n, uint32_t handler)
-{
+void idt_enable_gate(int interrupt) {
+    FLAG_SET(idt[interrupt].flags, IDT_FLAG_PRESENT);
+}
+
+void idt_disable_gate(int interrupt) {
+    FLAG_UNSET(idt[interrupt].flags, IDT_FLAG_PRESENT);
+}
+
+void set_idt_gate(int n, uint32_t handler) {
     idt[n].low_offset = low_16(handler);
     idt[n].sel = KERNEL_CS;
     idt[n].always0 = 0;
@@ -12,8 +19,7 @@ void set_idt_gate(int n, uint32_t handler)
     idt[n].high_offset = high_16(handler);
 }
 
-void set_idt()
-{
+void set_idt() {
     idt_reg.base = (uint64_t)&idt;
     idt_reg.limit = IDT_ENTRIES * sizeof(idt_gate_t) - 1;
     __asm__ __volatile__ ("lidtl (%0)" : : "r" (&idt_reg));
