1c25f620d0d90065dd3058263918fcecc6011538
deltadev-rsc <deltaqxq174@mail.ru>
2026-04-24 16:20:20 +0500

    Update ints.asm

--- kernel/cpu/asm/ints.asm
+++ kernel/cpu/asm/ints.asm
@@ -3,3 +3,23 @@
 
 extern isr_handler
 extern irq_handler
+
+
+%macro ISR_NOERRORCODE 1
+
+global ISR%1
+ISR%1:
+    push 0
+    push %1
+    jmp isr_common_stub
+
+%endmacro
+
+%macro ISR_ERRORCODE 1
+
+global isr%1
+isr%1:
+    push %1
+    hmp isr_common_stub
+
+%endmacro
