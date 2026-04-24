840e01b60c72a8717076feb99f72570624324c1c
deltadev-rsc <deltaqxq174@mail.ru>
2026-04-24 07:52:13 +0500

    Update build.sh

--- scripts/kern/build.sh
+++ scripts/kern/build.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/bash
 
 cd ~/OpenDelta/kernel/
-mkdir ./img
-mkdir ./obj
 
-function buildKernel() {
-    #---compile-asm-code---# 
-    nasm boot/i386/boot.asm -f bin -o img/boot.bin    
+mkdir -p img/
+mkdir -p obj/
+
+function build_i386 {
+    #---compile-asm-code---#
+    nasm boot/i386/boot.asm -f bin -o img/boot.bin
     nasm kernel_entry.asm -f elf -o obj/entry.o
-    
+    nasm arch/gdt/gdt.asm -f elf -o obj/gdtasm.o
+
     #---compile-kernel---#
     clang -m32 -march=i386 -fno-pie -ffreestanding -nostdlib -c kernel.c -o obj/kernel.o
     clang -m32 -march=i386 -fno-pie -ffreestanding -nostdlib -c cpu/idt.c -o obj/idt.o
@@ -18,15 +20,18 @@ function buildKernel() {
     clang -m32 -march=i386 -fno-pie -ffreestanding -nostdlib -c ports/ports.c -o obj/ports.o
     clang -m32 -march=i386 -fno-pie -ffreestanding -nostdlib -c drvs/screen.c -o obj/screen.o
     clang -m32 -march=i386 -fno-pie -ffreestanding -nostdlib -c lib/source/types.c -o obj/types.o
-        
+    clang -m32 -march=i386 -fno-pie -ffreestanding -nostdlib -c tty/tty.c -o obj/tty.o
+    clang -m32 -march=i386 -fno-pie -ffreestanding -nostdlib -c lib/source/ctype.c -o obj/ctype.o
+    clang -m32 -march=i386 -fno-pie -ffreestanding -nostdlib -c arch/gdt/gdt.c -o obj/gdt.o
+
     #---create-kernel-bin-file---#
-    ld.lld -m elf_i386 -s obj/kernel.o obj/stdbase.o obj/idt.o obj/mem.o obj/string.o obj/types.o obj/screen.o obj/ports.o obj/entry.o -o img/kernel.bin -z noexecstack -T link.ld -Ttext 0x10000 --oformat binary
+    ld.lld -m elf_i386 -s obj/kernel.o obj/stdbase.o obj/idt.o obj/mem.o obj/string.o obj/types.o obj/screen.o  obj/gdt.o obj/gdtasm.o obj/tty.o obj/ctype.o obj/ports.o obj/entry.o -o img/kernel.bin -z noexecstack -T link.ld -Ttext 0x10000 --oformat binary
 
     #---create-os-image---#
     dd if=/dev/zero of=img/open-delta.img bs=512 count=32516 status=none
-    mkfs.fat -F12 img/open-delta.img
-    dd if=img/boot.bin of=img/open-delta.img conv=ascii bs=1024 count=1 
+    mkfs.fat -F32 img/open-delta.img
+    dd if=img/boot.bin of=img/open-delta.img conv=ascii bs=1024 count=1
     dd if=img/kernel.bin of=img/open-delta.img conv=ascii bs=2048 count=1
 }
 
-buildKernel
+build_i386
