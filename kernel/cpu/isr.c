1046f59a1985d8d7864325fea468a0da628573a3
deltadev-rsc <deltaqxq174@mail.ru>
2026-04-24 16:19:38 +0500

    Update isr.c

--- kernel/cpu/isr.c
+++ kernel/cpu/isr.c
@@ -1,76 +1,75 @@
 #include "../lib/string.h"
-#include "../lib/types.h"
 #include "../lib/ports.h"
-#include "../lib/screen.h"
 #include "../lib/isr.h"
 #include "../lib/idt.h"
+#include "../lib/stdbase.h"
 
 isr_t interrupt_handlers[256];
 
 void isr_install()
 {
-	set_idt_gate(0, (uint32_t)isr0);
-	set_idt_gate(1, (uint32_t)isr1);
-	set_idt_gate(2, (uint32_t)isr2);
-	set_idt_gate(3, (uint32_t)isr3);
-	set_idt_gate(4, (uint32_t)isr4);
-	set_idt_gate(5, (uint32_t)isr5);
-	set_idt_gate(6, (uint32_t)isr6);
-	set_idt_gate(7, (uint32_t)isr7);
-	set_idt_gate(8, (uint32_t)isr8);
-	set_idt_gate(9, (uint32_t)isr9);
-	set_idt_gate(10, (uint32_t)isr10);
-	set_idt_gate(11, (uint32_t)isr11);
-	set_idt_gate(12, (uint32_t)isr12);
-	set_idt_gate(13, (uint32_t)isr13);
-	set_idt_gate(14, (uint32_t)isr14);
-	set_idt_gate(15, (uint32_t)isr15);
-	set_idt_gate(16, (uint32_t)isr16);
-	set_idt_gate(17, (uint32_t)isr17);
-	set_idt_gate(18, (uint32_t)isr18);
-	set_idt_gate(19, (uint32_t)isr19);
-	set_idt_gate(20, (uint32_t)isr20);
-	set_idt_gate(21, (uint32_t)isr21);
-	set_idt_gate(22, (uint32_t)isr22);
-	set_idt_gate(23, (uint32_t)isr23);
-	set_idt_gate(24, (uint32_t)isr24);
-	set_idt_gate(25, (uint32_t)isr25);
-	set_idt_gate(26, (uint32_t)isr26);
-	set_idt_gate(27, (uint32_t)isr27);
-	set_idt_gate(28, (uint32_t)isr28);
-	set_idt_gate(29, (uint32_t)isr29);
-	set_idt_gate(30, (uint32_t)isr30);
-	set_idt_gate(31, (uint32_t)isr31);
+	set_idt_gate(0, (uint64_t)isr0);
+	set_idt_gate(1, (uint64_t)isr1);
+	set_idt_gate(2, (uint64_t)isr2);
+	set_idt_gate(3, (uint64_t)isr3);
+	set_idt_gate(4, (uint64_t)isr4);
+	set_idt_gate(5, (uint64_t)isr5);
+	set_idt_gate(6, (uint64_t)isr6);
+	set_idt_gate(7, (uint64_t)isr7);
+	set_idt_gate(8, (uint64_t)isr8);
+	set_idt_gate(9, (uint64_t)isr9);
+	set_idt_gate(10, (uint64_t)isr10);
+	set_idt_gate(11, (uint64_t)isr11);
+	set_idt_gate(12, (uint64_t)isr12);
+	set_idt_gate(13, (uint64_t)isr13);
+	set_idt_gate(14, (uint64_t)isr14);
+	set_idt_gate(15, (uint64_t)isr15);
+	set_idt_gate(16, (uint64_t)isr16);
+	set_idt_gate(17, (uint64_t)isr17);
+	set_idt_gate(18, (uint64_t)isr18);
+	set_idt_gate(19, (uint64_t)isr19);
+	set_idt_gate(20, (uint64_t)isr20);
+	set_idt_gate(21, (uint64_t)isr21);
+	set_idt_gate(22, (uint64_t)isr22);
+	set_idt_gate(23, (uint64_t)isr23);
+	set_idt_gate(24, (uint64_t)isr24);
+	set_idt_gate(25, (uint64_t)isr25);
+	set_idt_gate(26, (uint64_t)isr26);
+	set_idt_gate(27, (uint64_t)isr27);
+	set_idt_gate(28, (uint64_t)isr28);
+	set_idt_gate(29, (uint64_t)isr29);
+	set_idt_gate(30, (uint64_t)isr30);
+	set_idt_gate(31, (uint64_t)isr31);
 
 	// Remap the PIC
-	port_byte_out(0x20, 0x11);
-	port_byte_out(0xA0, 0x11);
-	port_byte_out(0x21, 0x20);
-	port_byte_out(0xA1, 0x28);
-	port_byte_out(0x21, 0x04);
-	port_byte_out(0xA1, 0x02);
-	port_byte_out(0x21, 0x01);
-	port_byte_out(0xA1, 0x01);
-	port_byte_out(0x21, 0x0);
-	port_byte_out(0xA1, 0x0);
+	portByteOut(0x20, 0x11);
+	portByteOut(0xA0, 0x11);
+	portByteOut(0x21, 0x20);
+	portByteOut(0xA1, 0x28);
+	portByteOut(0x21, 0x04);
+	portByteOut(0xA1, 0x02);
+	portByteOut(0x21, 0x01);
+	portByteOut(0xA1, 0x01);
+	portByteOut(0x21, 0x0);
+	portByteOut(0xA1, 0x0);
 
 	// Install the IRQs
-	set_idt_gate(32, (uint32_t)irq0);
-	set_idt_gate(33, (uint32_t)irq1);
-	set_idt_gate(34, (uint32_t)irq2);
-	set_idt_gate(35, (uint32_t)irq3);
-	set_idt_gate(36, (uint32_t)irq4);
-	set_idt_gate(37, (uint32_t)irq5);
-	set_idt_gate(38, (uint32_t)irq6);
-	set_idt_gate(39, (uint32_t)irq7);
-	set_idt_gate(40, (uint32_t)irq8);
-	set_idt_gate(41, (uint32_t)irq9);
-	set_idt_gate(42, (uint32_t)irq10);
-	set_idt_gate(43, (uint32_t)irq11);
-	set_idt_gate(44, (uint32_t)irq12);
-	set_idt_gate(45, (uint32_t)irq13);
-	set_idt_gate(46, (uint32_t)irq14);
-	set_idt_gate(47, (uint32_t)irq15);
+	set_idt_gate(32, (uint64_t)irq0);
+	set_idt_gate(33, (uint64_t)irq1);
+	set_idt_gate(34, (uint64_t)irq2);
+	set_idt_gate(35, (uint64_t)irq3);
+	set_idt_gate(36, (uint64_t)irq4);
+	set_idt_gate(37, (uint64_t)irq5);
+	set_idt_gate(38, (uint64_t)irq6);
+	set_idt_gate(39, (uint64_t)irq7);
+	set_idt_gate(40, (uint64_t)irq8);
+	set_idt_gate(41, (uint64_t)irq9);
+	set_idt_gate(42, (uint64_t)irq10);
+	set_idt_gate(43, (uint64_t)irq11);
+	set_idt_gate(44, (uint64_t)irq12);
+	set_idt_gate(45, (uint64_t)irq13);
+	set_idt_gate(46, (uint64_t)irq14);
+	set_idt_gate(47, (uint64_t)irq15);
 
 	set_idt();
 }
@@ -116,14 +115,14 @@ char *exception_messages[] =
 
 void isr_handler(regs_t r)
 {
-	kprint("\n", 0x07);
-	kprint("received interrupt: ", 0x07);
+	prints("\n", 0x07);
+	prints("[received interrupt]: ", 0x07);
 	char s[3];
 	int_to_ascii(r.int_no, s);
-	kprint(s, 0x08);
-	kprint("\n", 0x07);
-	kprint(exception_messages[r.int_no], 0x08);
-	kprint("\n", 0x07);
+	prints(s, 0x08);
+	prints("\n", 0x07);
+	prints(exception_messages[r.int_no], 0x08);
+	prints("\n", 0x07);
 }
 
 void register_interrupt_handler(uint8_t n, isr_t handler)
@@ -134,10 +133,10 @@ void register_interrupt_handler(uint8_t n, isr_t handler)
 void irq_handler(regs_t r)
 {
 	if(r.int_no >= 40) {
-		port_byte_out(0xA0, 0x20); // slave
+		portByteOut(0xA0, 0x20); // slave
 	}
 
-	port_byte_out(0x20, 0x20); // master
+	portByteOut(0x20, 0x20); // master
 
 	if(interrupt_handlers[r.int_no] != 0) {
 		isr_t handler = interrupt_handlers[r.int_no];
