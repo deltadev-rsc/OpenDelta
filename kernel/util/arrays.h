ec89c078c55557241370656192c0190e620c0bd9
deltadev-rsc <deltaqxq174@mail.ru>
2026-04-24 21:39:00 +0500

    Create arrays.h

---
+++ kernel/util/arrays.h
@@ -0,0 +1,16 @@
+#ifndef ARRAYS_H
+#define ARRAYS_H
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define SIZE(array) (sizeof(array) / sizeof(array[0]))
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif 
