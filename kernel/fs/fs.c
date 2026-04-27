08564cbbdc1de067b4ae30cdaa25835b9201d91a
deltadev-rsc <deltaqxq174@mail.ru>
2026-04-27 21:21:58 +0500

    Update fs.c

--- kernel/fs/fs.c
+++ kernel/fs/fs.c
@@ -1,5 +1,6 @@
 #include "./headers/fs.h"
 #include "../lib/errno.h"
+#include "../lib/stdbase.h"
 
 uint32_t readFileSys(fs_node_t *node, uint64_t offset, uint32_t size, uint8_t *buffer) 
 {
@@ -28,5 +29,26 @@ uint32_t writeFileSys(fs_node_t *node, uint64_t offset, uint32_t size, uint8_t *
     }
     else {
         return -EROFS;
+    }
+}
+
+int vfs_write(fd_t *file, unsigned char *data, unsigned int size) 
+{
+    switch (*file) {
+        case stdin:
+            return 0;
+        case stdout:
+        case stderr: 
+            for (unsigned int i = 0; i < size; i++) {
+                _putc(data[i], (FILE *)file);
+            }
+            return size;
+        case debug: 
+            for (unsigned int i = 0; i < size; i++) {
+                e9_putc(data[i]);
+            }
+            return size;
+        default:
+            return -1;
     }
 }
