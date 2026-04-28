7c7bf58ba3bbcef549b648af45a027de23a307da
deltadev-rsc <deltaqxq174@mail.ru>
2026-04-28 18:33:10 +0500

    Create fpu.c

---
+++ kernel/fpu/fpu.c
@@ -0,0 +1,97 @@
+#include "fpu.h"
+#include "../lib/isr.h"
+#include "../lib/system.h"
+#include "../mem/header/memory.h"
+
+#define NO_LAZY_FPU
+
+sys_process_t *fpu_thread = NULL;
+sys_process_t *current_proc = NULL;
+
+void set_fpu_cw(const uint16_t cw) {
+    __asm__ volatile ("fldcw %0" :: "m"(cw));
+}
+
+void enable_fpu(void) 
+{
+    __asm__ volatile ("clts");
+    unsigned int t;
+    __asm__ volatile ("mov %%cr0, %0" : "=r"(t));
+    t &= ~(1 << 2);
+    t |= (1 << 1);
+    __asm__ volatile ("mov %0, %%cr0" :: "r"(t));
+
+    __asm__ volatile ("mov %%cr4, %0" : "=r"(t));
+    t |= 3 << 9;
+    __asm__ volatile ("mov %0, %%cr4" :: "r"(t));
+}
+
+void disable_fpu(void) {
+    unsigned int t;
+    __asm__ volatile ("mov %%cr0, %0" : "=r"(t));
+    t |= 1 << 3;
+    __asm__ volatile ("mov %0, %%cr0" :: "r"(t));
+}
+
+uint8_t saves[512] __attribute__((aligned(16)));
+
+void restore_fpu(sys_process_t *proc) {
+    memcpy(&saves, (uint8_t *)&proc->theard.fp_regs, 512);
+    __asm__ volatile ("fxrstor (%0)" :: "r"(saves));
+}
+
+void save_fpu(sys_process_t *proc) {
+    __asm__ volatile ("fxsave (%0)" :: "r"(saves));
+    memcpy((uint8_t *)&proc->theard.fp_regs, &saves, 512);
+}
+
+void init_fpu(void) {
+    __asm__ volatile ("fninit");
+}
+
+void invalid_op(void) 
+{
+    enable_fpu();
+    if (fpu_thread == current_proc) {
+        return;
+    }
+
+    if (fpu_thread) {
+        save_fpu(fpu_thread);
+    }
+
+    fpu_thread = (sys_process_t *)current_proc;
+    if (!fpu_thread->theard.fpu_enabled) {
+        init_fpu();
+        fpu_thread->theard.fpu_enabled = 1;
+        return;
+    }
+
+    restore_fpu(fpu_thread);
+}
+
+void switch_fpu(void) {
+    #ifdef NO_LAZY_FPU
+        save_fpu((sys_process_t *)current_proc);
+    #else
+        disable_fpu();
+    #endif
+}
+
+void unswitch_fpu(void) {
+    #ifdef NO_LAZY_FPU
+        restore_fpu((sys_process_t *)current_proc);
+    #endif
+}
+
+void fpu_install(void) {
+    #ifdef NO_LAZY_FPU
+        enable_fpu();
+        init_fpu();
+        save_fpu((void *)current_proc);
+    #else 
+        enable_fpu();
+        disable_fpu();
+        register_interrupt_handler(7, &invalid_op);
+    #endif
+}
